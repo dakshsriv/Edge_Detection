15779822e2775c97b0f6fa33523fbf505771439b
Daksh Srivastava <daksh.srivastava@gmail.com>
2022-04-16 18:35:32 -0400

    Convolution-style parsing of edges done.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-sample = [[1,2,3], 
-          [2,3,4], 
-          [3,4,5]]
+from pprint import pprint
+sample = [[10,20,200,200], 
+          [20,30,145,100], 
+          [30,40,255,230],
+          [10,20,100,100]]
 
 def calcDeriv(table):
+    pprint(table)
     Deriv_N = 5*(table[0][0] + table[0][1] + table[0][2]) - 3*(table[1][0] + table[1][2] + table[2][0] + table[2][1] + table[2][2])
     Deriv_NE = 5*(table[0][1] + table[0][2] + table[1][2]) - 3*(table[0][0] + table[1][0] + table[2][0] + table[2][1] + table[2][2])
     Deriv_E = 5*(table[0][2] + table[1][2] + table[2][2]) - 3*(table[0][1] + table[0][0] + table[1][0] + table[2][0] + table[2][1])
@@ -18,7 +21,18 @@ def calcDeriv(table):
         if v > max_number:
             max_number = v
             direction = k
+    if max_number <= 800:
+        direction = "no edge"
     return derivDict, direction
 
+
 if __name__ == "__main__":
-    print(calcDeriv(sample))
+    sendTable = [[0,0,0],[0,0,0],[0,0,0]]
+    for i in range(1, len(sample)-1):
+        for j in range (1, len(sample)-1):
+            for m in range(0, 3):
+                for n in range(0, 3):
+                    print(i, j, m, n)
+                    sendTable[m][n] = sample[i+m-1][j+n-1]
+            print(calcDeriv(sendTable))
+            sendTable=[[0,0,0],[0,0,0],[0,0,0]]
